408058d8f6d22dfdae99e1564f885de382ab7f41
Nikita Popov <n.r.popov@tinkoff.ru>
2022-10-02 17:48:53 +0400

    lab1: update

--- lab-1/src/models/SmallSet.java
+++ lab-1/src/models/SmallSet.java
@@ -3,45 +3,103 @@ package models;
 import java.util.*;
 
 public class SmallSet<T> implements Set<T> {
-    private int size = 0;
+    private T[] values;
+    private int[] indexes;
 
     public SmallSet(int size) {
-        this.size = size;
+        values = (T[]) new Object[size];
+        indexes = new int[size];
+        Arrays.fill(indexes, -1);
     }
 
     @Override
     public int size() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
+        return values.length;
     }
 
     @Override
     public boolean contains(Object o) {
+        if (o != null) {
+            int hash = getHashCode(o);
+
+            if (indexes[hash] == -1) {
+                System.out.print(o + ":");
+                System.out.println(values[hash]);
+                return values[hash].equals(o);
+            }
+
+            // TODO
+            int currentIndex = hash;
+            int attempt = 0;
+            while (indexes[currentIndex] != -1 && attempt++ < values.length) {
+                currentIndex = indexes[currentIndex];
+            }
+            if (attempt >= size()) {
+                return  false;
+            }
+
+            return values[currentIndex].equals(o);
+        }
         return false;
     }
 
     @Override
     public boolean add(T t) {
-        return false;
+        int hash = getHashCode(t);
+
+        if (values[hash] == null) {
+            values[hash] = t;
+            return true;
+        }
+
+        int currentIndex = hash;
+        int attempt = 0;
+        while (indexes[currentIndex] != -1 && attempt++ < values.length) {
+            currentIndex = indexes[currentIndex];
+        }
+        if (attempt >= size()) {
+            return  false;
+        }
+
+        indexes[currentIndex] = findEmptyIndex(currentIndex);
+        values[indexes[currentIndex]] = t;
+
+        return true;
+    }
+
+    private int findEmptyIndex(int from) {
+        for (int i = from % size(); i < size(); i ++) {
+            if (values[i] == null) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private int getHashCode(Object element) {
+        int hash = element.hashCode();
+        return hash % size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return Arrays.stream(values).noneMatch(Objects::nonNull);
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return Arrays.stream(this.values).iterator();
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return this.values;
     }
 
     @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
+    public <M> M[] toArray(M[] a) {
+        return a;
     }
 
     @Override
@@ -71,6 +129,7 @@ public class SmallSet<T> implements Set<T> {
 
     @Override
     public void clear() {
-
+        values = (T[]) new Object[values.length];
+        indexes = new int[indexes.length];
     }
 }
