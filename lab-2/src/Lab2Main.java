738e5f1688f071f06706a7697e619afbcebdd81b
John Pier <warhamer10tw@gmail.com>
2022-10-16 21:01:45 +0400

    upd

--- lab-2/src/Lab2Main.java
+++ lab-2/src/Lab2Main.java
@@ -1,5 +1,45 @@
+import com.johnpier.Main;
+
 public class Lab2Main {
     public static void main(String[] args) {
         // 1-7, 3-5
+        runFirst();
+    }
+
+    // 1-7
+    private static void runFirst() {
+        final int size = 4;
+        int[][] image = new int[size][size];
+
+        int num = 10;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                image[i][j] = num++;
+            }
+        }
+
+        pintImg(image, size);
+
+        for (int i = 0; i < (int) Math.ceil(size / 2.0); i++) {
+            for (int j = 0; j < (int) Math.floor(size / 2.0); j++) {
+                int tmp = image[i][j];
+                image[i][j] = image[j][size - 1 - i];
+                image[j][size - 1 - i] = image[size - 1 - i][size - 1 - j];
+                image[size - 1 - i][size - 1 - j] = image[size - 1 - j][i];
+                image[size - 1 - j][i] = tmp;
+            }
+        }
+        System.out.println("old -> new");
+
+        pintImg(image, size);
+    }
+
+    private static void pintImg(int[][] image, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(image[i][j] + "\t");
+            }
+            System.out.println("\t");
+        }
     }
 }
