03c822ae9bbe328218cd16b6728f90c1015b38f9
Nikita Popov <n.r.popov@tinkoff.ru>
2022-11-15 22:42:02 +0400

    lab4: upd

--- lab-4/src/Main.java
+++ lab-4/src/Main.java
@@ -2,13 +2,18 @@ public class Main {
     // 8
     public static void main(String[] args) {
         System.out.println("Lab 4. Task 8");
-
-        drawLine(Image, 16, 5, 16, 4);
+        System.out.println("Test line");
+        byte start = -128;
+        System.out.println(Integer.toBinaryString(setBytes(start, 2, 7)));
+        System.out.println("Test image");
+        printImage(Image, 16);
+        drawLine(Image, 16, 0, 14, 3); // oт 0 отсчет
+        System.out.println("--------\t\t--------");
         printImage(Image, 16);
     }
 
 
-    private static final byte[] Image = new byte[]{0b1011111, 0b1000000, 0b0001001, 0b0001001, 0b0010001, 0b1010111, 0b1001000, 0b0000001, 0b0100001, 0b0110001};
+    private static final byte[] Image = new byte[]{-128, -128, -128, -128, -128, -128, -128, -128, -128, -128};
 
     /**
      * Содержимое монохромного экрана хранится в одномерном массиве байтов,
@@ -19,25 +24,45 @@ public class Main {
      * Сигнатура метода должна выглядеть примерно так:
      * drawLine(byte[] screen, int width, int x1, int х2, int у)
      */
-
-
     private static void drawLine(byte[] screen, int width, int x1, int x2, int y) {
-        int height = screen.length / (width / 8);
         int realWidth = width / 8;
-        byte mask = 0b1111111;
+        int dx = x2 - x1;
+        int i = x1;
+        while (dx >= 0) {
+            int index = i / 8;
+            int di = i % 8;
+            byte element = screen[y*realWidth + index];
+            if (dx <= (8 - di)) {
+                element = setBytes(element, di, di + dx);
+                dx = -1;
+            }
+            else {
+                element = setBytes(element, di, 8);
+                i += 8 - di;
+                dx -= 8 - di;
+            }
+            screen[y*realWidth + index] = element;
+        }
+    }
 
-        for (int x = x1; x <= x2; x++){
-            screen[x + realWidth * (x / realWidth)] = 0; // TODO:
-        };
+    private static byte setBytes(byte value, int i1, int i2) {
+        int mask1 = (1 << (8 - i1)) - 1;
+        int mask2 = (1 << (7 - i2)) - 1;
+//        System.out.println(Integer.toBinaryString(value));
+//        System.out.println(Integer.toBinaryString(mask1));
+//        System.out.println(Integer.toBinaryString(mask2));
+//        System.out.println(Integer.toBinaryString(mask1 ^ mask2));
+//        System.out.println(Integer.toBinaryString(value | (mask1 ^ mask2)));
+        return (byte) (value | (mask1 ^ mask2));
     }
 
     private static void printImage(byte[] screen, int width) {
         int realWidth = width / 8;
         for (int i = 0; i < screen.length; i++) {
             if ((i + 1) % realWidth == 0) {
-                System.out.println(Integer.toBinaryString(screen[i]));
+                System.out.println(Integer.toBinaryString(screen[i]).substring(24));
             } else {
-                System.out.print(Integer.toBinaryString(screen[i]) + "\t\t");
+                System.out.print(Integer.toBinaryString(screen[i]).substring(24) + "\t\t");
             }
         }
     }
