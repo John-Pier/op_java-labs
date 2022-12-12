1948a0b4287caf0c475748b89ed03526d2249376
Nikita Popov <n.r.popov@tinkoff.ru>
2022-12-13 02:20:47 +0400

    lab6: upd

--- lab-6/src/Main.java
+++ lab-6/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     //5 - онлайн библиотека
+    // Разработайте структуры данных для онлайн-библиотеки.
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
